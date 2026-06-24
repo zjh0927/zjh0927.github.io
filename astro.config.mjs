32c5d5236778a8c908bdb38f3ed251e8a628577b
Zeng Jiahao <3083448680@qq.com>
2026-06-24 16:08:03 +0800

    Fix deploy workflow and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://zengjiahao.github.io',
+	site: 'https://zjh0927.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
